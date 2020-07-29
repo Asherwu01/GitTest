6540bdfc93ce25b550b4224812a3ad69b2c76300
Asherwu01 <46586057+Asherwu01@users.noreply.github.com>
2020-07-29 17:34:35 +0800

    Update GitTest.java
    update

--- src/main/java/com/asher/git/GitTest.java
+++ src/main/java/com/asher/git/GitTest.java
@@ -2,6 +2,8 @@ package com.asher.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        //打印
         System.out.println("This is a Git Project Test");
+        
     }
 }
